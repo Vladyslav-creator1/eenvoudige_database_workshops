cbdb46cade661a215854a8dcc7a2621c10f10967
Vlad <vlad.lytvychenko@edu-kw1c.nl>
2026-03-05 10:11:30 +0100

    includs commit

---
+++ Workshops/week 4/introductieDB.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Autor: L.Vlad
+ * Date: 5 3 26
+ *
+ * Verbinding maken met een database en gegevens ophalen.
+ */
+
+//includen van db_functies
+include("/includs/db_functions.php");
