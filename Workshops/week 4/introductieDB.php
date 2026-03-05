b2b789e96aabdd39a42ca41c097992cd31b93522
Vlad <vlad.lytvychenko@edu-kw1c.nl>
2026-03-05 10:27:23 +0100

    includs2 commit

--- Workshops/week 4/introductieDB.php
+++ Workshops/week 4/introductieDB.php
@@ -7,4 +7,19 @@
  */
 
 //includen van db_functies
-include("/includs/db_functions.php");
+include "../includs/db_functions.php";
+
+//databaseverbinding starten
+StartConnection("world");
+
+//Schrijven van de Query
+$query = "SELECT * FROM country;";
+
+//Uitvoeren van de Query
+
+$result = ExecuteSelectQuery($query);
+//loop resultaten
+
+foreach ($result as $row) {
+    echo $row["name"] . "<br>";
+}
